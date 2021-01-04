9ef7ce37b653030751ae3011fdb0c2f83741d545
Ahmed Samy <aahmedsamy.as@gmail.com>
2021-01-04 16:15:56 +0200

    added .env for project variables

--- news_blog/settings.py
+++ news_blog/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+import environ
+
+env = environ.Env(
+    DEBUG_MODE=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = f"{BASE_DIR}/templates"
